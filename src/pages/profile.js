d2236080fdd25ad12140820ebe7257a51d8172ee
Nwani Victory <vickywane@gmail.com>
2019-05-24 23:45:30 +0100

    .

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
 import styled from "styled-components"
 import Image from "../components/image"
 import { media } from "../media_style"
+import { Modal, Button } from "react-bootstrap"
+import Flex from "styled-flex-component"
 
 const Div = styled.div`
   ${media.tablet`
@@ -72,23 +73,120 @@ const Name = styled.h1`
   `}
 `
 
-const IndexPage = () => (
-  <Div>
-    <div
-      style={{
-        width: "100vw",
-        height: "55vh",
-        position: "relative",
-        top: "-2px",
-      }}
-    >
-      <Name> Nwani Victory </Name>
-      <Text> Software Engineer from Lagos , Nigeria , Africa. </Text>
-      <Github> CONTACT ME </Github>
+const Message = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 1.1px solid green;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  font-size : 1.1em
+  width : 70%
+  height : 60px
+  font-weight : bold
+	&:hover {
+    cursor: pointer;
+    color: white;
+    background: green;
+  }
+  ${media.tablet`
+  font-size : 1em
+  width : 85%
+`}
+  ${media.phone`
+  width : 45%
+`}
+`
 
-      <Image />
-    </div>
-  </Div>
-)
+const Mail = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 1.1px solid red;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  font-size : 1.1em
+  width : 70%
+  height : 60px
+  font-weight : bold
+	&:hover {
+    cursor: pointer;
+    color: white;
+    background: red;
+  }
+  ${media.tablet`
+  font-size : 1em
+  width : 85%
+`}
+  ${media.phone`
+  width : 45%
+`}
+`
 
-export default IndexPage
+class MyVerticallyCenteredModal extends React.Component {
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title  >
+            <h5> Contact Me </h5>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h5> You can get across to me through the following mediums </h5>{" "}
+          <br />
+          <Flex column>
+            <Message> Whatsapp Message </Message> <br />
+            <Mail> Send Email </Mail>
+          </Flex>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide} > CLOSE </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
+
+class Profile extends Component {
+  state = {
+    modal: false,
+  }
+
+  render() {
+    let modalClose = () => this.setState({ modal: false })
+    return (
+      <Div>
+        <div
+          style={{
+            width: "100vw",
+            height: "55vh",
+            position: "relative",
+            top: "-2px",
+          }}
+        >
+          <Name> Nwani Victory </Name>
+          <Text> Software Engineer from Lagos , Nigeria , Africa. </Text>
+          <Github onClick={() => this.setState({ modal: true })}>
+            {" "}
+            CONTACT ME{" "}
+          </Github>
+
+          <Image />
+
+          <MyVerticallyCenteredModal
+            show={this.state.modal}
+            onHide={modalClose}
+          />
+        </div>
+      </Div>
+    )
+  }
+}
+
+export default Profile
