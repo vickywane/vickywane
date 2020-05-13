1f6ff183b070d71d2e767311f3610a06aefb764b
Nwani Victory <vickywane@gmail.com>
2020-05-13 14:41:20 +0100

    filesystem err fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
