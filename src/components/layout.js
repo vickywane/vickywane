5f81147c418757bb2e4438f1f9a98faa8243a92a
Vickywane <vickywane@gmail.com>
2020-11-05 19:28:16 +0100

    font change

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
 import Header from "./header"
 import "./layout.css"
@@ -23,7 +24,26 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <main>{children}</main>{" "}
+    </div>
+  )
 }
 
 Layout.propTypes = {
