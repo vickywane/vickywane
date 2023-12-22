4398c9acf2c7cb977d2fdbba96f63138618c0077
Victory Nwani <vickywane@gmail.com>
2023-12-22 12:13:09 +0000

    feat: install google-tag-manager into application

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
+import { GoogleTagManager } from '@next/third-parties/google'
+
+const gtmId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
-    )
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+
+      <GoogleTagManager gtmId={gtmId} />
+    </Html>
+  )
 }
