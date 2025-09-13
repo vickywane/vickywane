b200e79e5acd300b91fa6948d93018aa48c9467b
Nwani Victory <vickywane@gmail.com>
2025-09-13 21:55:03 +0100

    update: modify sitemap output

--- apps/frontend/app/sitemap.ts
+++ apps/frontend/app/sitemap.ts
@@ -1,27 +1,29 @@
 import { SITEMAP_QUERY } from "@/data/gqols"
 import { SanityClient } from "@/utils/Sanity"
-import { type MetadataRoute } from 'next';
+import { MetadataRoute } from "next"
 
 const SITE_URL = process.env.NEXT_PUBLIC_DOMAIN
 
-export default async function sitemap() : Promise<MetadataRoute.Sitemap>{
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   if (!SITE_URL) {
     throw new Error("SITE_URL is not defined in environment variables")
   }
 
   const articles = await SanityClient().fetch(SITEMAP_QUERY)
 
-  const articleUrls = articles.map(item => {
+  const articleUrls = articles.map((item: any) => {
     if (item?.articles) {
       return {
         url: `${SITE_URL}/blog/category/${item.slug.current}`,
-        lastModified: item.publish_date,
+        lastModified: new Date(item.publish_date),
+        priority: 1,
       }
     }
 
     return {
       url: `${SITE_URL}/blog/${item.slug.current}`,
-      lastModified: item.publish_date,
+      lastModified: new Date(item.publish_date),
+      priority: 1,
     }
   })
 
@@ -29,6 +31,7 @@ export default async function sitemap() : Promise<MetadataRoute.Sitemap>{
     {
       url: SITE_URL,
       lastModified: new Date().toISOString(),
+      priority: 1,
     },
     ...articleUrls,
   ]
